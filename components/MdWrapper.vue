1cac538d8aa8f63119aca0ac7e9f8d833cd88855
Magdolna Vargáné Pusztai <magdolna.varganepusztai@innonic.com>
2020-02-12 22:08:42 +0100

    Initial commit

---
+++ components/MdWrapper.vue
@@ -0,0 +1,8 @@
+<template >
+<div v-html="body"></div>
+</template>
+<script>
+export default {
+    props: ['body']
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+
+    <component :is="getLayout" :allitems="allBlogPosts"></component>
+</template>
+
+<script>
+import BaelGrid from "~/components/BaelGrid";
+import FullGrid from "~/components/FullGrid";
+export default {
+    watchQuery: ['page'],
+
+   transition (to, from) {
+     
+    if (!from) return 'fade'
+    return +to.query.page > +from.query.page ? 'slide-right' : 'slide-left'
+  },
+  name: "Index",
+  components: { BaelGrid,FullGrid },
+  data() {
+    return {};
+  },
+  methods: {},
+
+  computed: {
+    allBlogPosts() {
+      return this.$store.state.blogPosts;
+    },
+    getLayout() {
+if (this.$store.state.siteInfo.altlayout == false ) {
+  return 'BaelGrid'
+} else if (this.$store.state.siteInfo.altlayout == true ) {
+  return 'FullGrid'
+}
+
+    }
+  }
+};
+</script>
+
+<style>
+
+.browse a {
+  width: 100%;
+}
+.search:focus {
+  outline: none;
+}
+.footer__heading {
+  text-transform: uppercase;
+}
+nav .r {
+  grid-gap: 0;
+}
+.r.full-height {
+  grid-gap: 0;
+}
+@media only screen and (max-width: 40rem) {
+  .xs-collapse {
+    visibility: hidden;
+    visibility: collapse;
+    border: 0 !important;
+    border-color: none !important;
+    padding: 0 !important;
+  }
+  .xs-visible {
+    visibility: visible;
+  }
+}
+</style>
